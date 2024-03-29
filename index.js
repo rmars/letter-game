bba125e61f7f2bff12940c1eba872b828c666a9c
R. Mars <549258+rmars@users.noreply.github.com>
2024-03-28 19:56:34 -0700

    Implement Undo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("myCanvas");
 const undoBtn = document.getElementById("undo");
 const ctx = canvas.getContext("2d");
 
-const letters = [
+const puzzle = [
   ["A", "B", "C"], // top
   ["D", "E", "F"], // right
   ["G", "H", "I"], // bottom
@@ -13,6 +13,8 @@ ctx.font = "30px Comic Sans MS";
 ctx.fillStyle = "#28a745";
 ctx.textAlign = "center";
 
+const endpointRadius = 4;
+
 const cWidth = canvas.width;
 const cHeight = canvas.height;
 const widthUnit = canvas.width / 4;
@@ -21,25 +23,30 @@ const heightUnit = canvas.height / 4;
 const topMargin = cHeight / 8;
 const sideMargin = cWidth / 8;
 
-// top
-for (l in letters[0]) {
-  ctx.fillText(letters[0][l], widthUnit + l * widthUnit, topMargin);
-}
+const drawLetters = letters => {
+  // top
+  for (l in letters[0]) {
+    ctx.fillText(letters[0][l], widthUnit + l * widthUnit, topMargin);
+  }
+  // right
+  for (l in letters[1]) {
+    ctx.fillText(
+      letters[1][l],
+      cWidth - sideMargin,
+      heightUnit + l * heightUnit
+    );
+  }
+  // bottom
+  for (l in letters[2]) {
+    ctx.fillText(letters[2][l], widthUnit + l * widthUnit, cHeight - topMargin);
+  }
+  // left
+  for (l in letters[3]) {
+    ctx.fillText(letters[3][l], sideMargin, heightUnit + l * heightUnit);
+  }
+};
 
-// right
-for (l in letters[1]) {
-  ctx.fillText(letters[1][l], cWidth - sideMargin, heightUnit + l * heightUnit);
-}
-
-// bottom
-for (l in letters[2]) {
-  ctx.fillText(letters[2][l], widthUnit + l * widthUnit, cHeight - topMargin);
-}
-
-// left
-for (l in letters[3]) {
-  ctx.fillText(letters[3][l], sideMargin, heightUnit + l * heightUnit);
-}
+drawLetters(puzzle);
 
 let clicks = 0;
 const line = {
@@ -49,7 +56,8 @@ const line = {
   toY: null,
 };
 
-const linesDrawn = [];
+let linesDrawn = [];
+
 const drawLine = ({ fromX, fromY, toX, toY }) => {
   ctx.beginPath();
   ctx.moveTo(fromX, fromY);
@@ -57,28 +65,41 @@ const drawLine = ({ fromX, fromY, toX, toY }) => {
   ctx.stroke();
 };
 
-const eraseLastLineDrawn = () => {
-  ctx.clearRect(topMargin, sideMargin, cHeight - topMargin, cWidth - topMargin);
-};
-
 const handleCanvasClick = e => {
   clicks++;
   const [x, y] = [e.offsetX, e.offsetY];
-  console.log(x, y);
   if (clicks % 2 == 0) {
     line.toX = x;
     line.toY = y;
 
     drawLine(line);
-    linesDrawn.push(line);
+    drawCircle(x, y, "green");
+    // if I don't re-assign here, it pushes the original line over and over
+    linesDrawn.push({
+      fromX: line.fromX,
+      fromY: line.fromY,
+      toX: line.toX,
+      toY: line.toY,
+    });
   } else {
     line.fromX = x;
     line.fromY = y;
+    drawCircle(x, y, "yellow");
   }
 };
 
+const drawCircle = (x, y, color) => {
+  ctx.beginPath();
+  ctx.arc(x, y, endpointRadius, 0, 2 * Math.PI, false);
+  ctx.fillStyle = color;
+  ctx.fill();
+};
+
 const handleUndoBtnClick = () => {
-  console.log("clicked");
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
+  linesDrawn.pop();
+  drawLetters(puzzle); // redraw letters
+  linesDrawn.forEach(drawLine); // redraw line
 };
 
 canvas.addEventListener("click", handleCanvasClick, false);
